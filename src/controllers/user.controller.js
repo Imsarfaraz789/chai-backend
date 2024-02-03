a918fc744ac47c0c2c4286b673e0bdfc224e0919
Imsarfaraz786 <sarfaraztech786@gmail.com>
2024-02-03 10:56:27 +0530

    all configuration are done

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,13 @@
+import asyncHandler from "../utils/asyncHandler.js"
+
+
+const register = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: "chai or code"
+    })
+})
+
+
+
+
+export default register
